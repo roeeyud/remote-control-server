5004f25ea8b0f6e44c37c710202227f79002f323
roeeyud <roeeyud@gmail.com>
2020-04-07 09:42:14 +0300

    Added encryption support

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const WebSocket = require('ws');
+const CryptoJS = require("crypto-js");
+
 const port = process.env.PORT || 5000;
 
 const wss = new WebSocket.Server({ port });
@@ -6,7 +8,14 @@ const wss = new WebSocket.Server({ port });
 wss.on('connection', function connection(ws) {
     console.log('connected');
     ws.on('message', function incoming(message) {
-        console.log(`received: ${message}`);
+        try {
+            const bytesBody  = CryptoJS.AES.decrypt(message, 'abcd');
+            const body = JSON.parse(bytesBody.toString(CryptoJS.enc.Utf8));
+            console.log(`received: `, body);
+        } catch (e) {
+            console.error(e);
+            ws.close(1002, 'Invalid Password');
+        }
     });
 });
 
